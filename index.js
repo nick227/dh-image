d6c9334b738e70586dcaddb2f3981b49b8ddca6a
Nicholas Rios <nicholas.jay.rios@gmail.com>
2022-01-28 02:07:31 -0600

    bbb

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 var express = require("express");
-var fs = require("fs");
-var app = express();
-var port = process.env.PORT || 3000
 var bodyParser = require('body-parser')
+var fs = require("fs");
+var port = process.env.PORT || 3000
+var app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.listen(port, () => {});
 app.get("/events", (req, res, next) => {
-    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"])
+    const path = './dh-image-log.txt'
+    res.json(read(path))
 });
-
 app.post("/event", (req, res, next) => {
-    save(JSON.stringify(req.body))
+    save(req.body)
     res.json(["ok"])
 });
 
-
 function read(path) {
-    return fs.readFileSync(path)
+    return fs.readFileSync(path, 'utf8')
 }
 
 function save(data) {
     const path = './dh-image-log.txt'
-    fs.writeFile(path, data, { flag: 'a' }, function(err) {
+    const cdata = JSON.parse(read(path))
+    cdata.push(data)
+    fs.writeFile(path, JSON.stringify(cdata), { flag: 'w' }, function(err) {
         if (err) throw err;
     });
 }
