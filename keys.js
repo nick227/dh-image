d4c37d6085a9142dd1680d955ae1dc060fa5e882
Nicholas Rios <nicholas.jay.rios@gmail.com>
2022-02-08 18:59:48 -0600

    add

---
+++ keys.js
@@ -0,0 +1,28 @@
+const API_KEYS = {
+    icon8: {
+        icons: '7c6b363c45b217cfd6fe280f4de7861691550354',
+        vectors: 'lDKfyPRkhwP9Zvx9iGuJOVXXIaotTU6umndAJwNF',
+        photos: 'Zy9WRJFistH6N5vQjKkoRSQjp7QCCpZx2eGfDUHe',
+        music: 'Z0GlyHUtVQSclMhSq9tnjuH5Jp9vixdZAwZdIezN'
+    },
+    noun: {
+        key: '6fe5896299e4454da024e88e25d06dea',
+        secret: 'a1106661e3e7492785bee89ca506519a'
+    },
+    biglabs: {
+        key: '045145eebbf50c7e68cad637c21e6608'
+    },
+    flatIcon: {
+        key: '814f937125367cee6b1945e5d15b8035b1deb15a'
+    },
+    mysql: {
+        auth: {
+            host: 'dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            user: 'zd6skoui21wh9q14',
+            password: 'fvz871p26r5wghnk',
+            database: 'eepab774vfog2vyw'
+        }
+    }
+}
+
+module.exports = API_KEYS
