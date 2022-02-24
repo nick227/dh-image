d45cfa0e630cfecfd00345f709674527bc8402cd
Nicholas Rios <nicholas.jay.rios@gmail.com>
2022-02-23 19:58:01 -0600

    ya

--- api/shutterstock.js
+++ api/shutterstock.js
@@ -5,22 +5,11 @@ module.exports = function(req, res, next) {
     sstk.setAccessToken(API_KEYS.shutterstock.token);
     const api = new sstk.ImagesApi();
 
-    const queryParams = {
-        query: 'New York',
-        sort: 'popular',
-        orientation: 'horizontal',
-        per_page: 3
-    };
-
     api.searchImages(req.query)
         .then(function({ data }) {
-            console.log(data);
             res.json({ "results": data })
         })
         .catch(function(error) {
-            console.error('error');
-            console.error(error);
-            res.json(error)
-            console.error('error');
+            res.send(error)
         });
 }
